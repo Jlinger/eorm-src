fde27454e7e1ace2b7b4dbdf4c2a7b8718b1f299
Qingshan Luo <shanshan.lqs@gmail.com>
2017-01-17 00:02:20 +0800

    Update local test model.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,12 +16,21 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // Add MySQL server connection.
-Eorm\Server::add(
-    function () {
-        return new PDO('mysql:host=127.0.0.1;port=3306;dbname=eorm;charset=utf8', 'root', '');
-    },
-    'eorm'
-);
+if (file_exists(__DIR__ . '/../test.lock')) {
+    Eorm\Server::add(
+        function () {
+            return new PDO('mysql:host=127.0.0.1;port=3306;dbname=eorm;charset=utf8', 'eorm', 'eorm');
+        },
+        'eorm'
+    );
+} else {
+    Eorm\Server::add(
+        function () {
+            return new PDO('mysql:host=127.0.0.1;port=3306;dbname=eorm;charset=utf8', 'root', '');
+        },
+        'eorm'
+    );
+}
 
 // Load test model.
 require __DIR__ . '/../src/Models/Example.php';
