237a0a5c35ce36171b824e4f08b13301455629b5
Qingshan Luo <shanshan.lqs@gmail.com>
2017-01-17 00:28:29 +0800

    Add some unit test.

--- tests/EormTest/EormTest.php
+++ tests/EormTest/EormTest.php
@@ -14,12 +14,84 @@
  */
 namespace EormTest;
 
+use Models\Example;
 use PHPUnit\Framework\TestCase;
 
 class EormTest extends TestCase
 {
-    public function testExample()
+    public function testGetTable()
     {
-        $this->assertTrue(true);
+        $this->assertEquals(
+            'example',
+            Example::getTable()
+        );
+    }
+
+    public function testGetPrimaryKey()
+    {
+        $this->assertEquals(
+            'id',
+            Example::getPrimaryKey()
+        );
+    }
+
+    public function testClean()
+    {
+        $this->assertTrue(Example::clean());
+    }
+
+    public function testInsert()
+    {
+        $this->assertEquals(
+            1,
+            Example::insert([
+                'name'    => 'Test1',
+                'age'     => 10,
+                'country' => 'China',
+            ])
+        );
+    }
+
+    public function testFind()
+    {
+        $this->assertEquals(
+            [
+                ['id' => '1', 'name' => 'Test1', 'age' => '10', 'country' => 'China'],
+            ],
+            Example::find(1)->result()->toArray()
+        );
+    }
+
+    public function testCreate()
+    {
+        $this->assertEquals(
+            [
+                ['id' => '2', 'name' => 'Test2', 'age' => '20', 'country' => 'China'],
+            ],
+            Example::create([
+                'name'    => 'Test2',
+                'age'     => 20,
+                'country' => 'China',
+            ])->result()->toArray()
+        );
+    }
+
+    public function testAll()
+    {
+        $this->assertEquals(
+            [
+                ['id' => '1', 'name' => 'Test1', 'age' => '10', 'country' => 'China'],
+                ['id' => '2', 'name' => 'Test2', 'age' => '20', 'country' => 'China'],
+            ],
+            Example::all()->result()->toArray()
+        );
+    }
+
+    public function testCount()
+    {
+        $this->assertEquals(
+            2,
+            Example::count()
+        );
     }
 }
